dc1ed7e4f359c86b18dc3611ea19f4eff5756b91
mahmoudshokryaxisapp <mshokry@axisapp.com>
2024-06-27 20:54:09 +0300

    adding seeder to seed Data from CSV file

---
+++ src/models/movies.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import { getMoviesDb } from "../adapters/mongodb-manager";
+import * as uuid from "uuid";
+
+const movieSchema = new mongoose.Schema(
+  {
+    movieId: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+      default: uuid.v7,
+    },
+    title: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    director: {
+      type: String,
+    },
+    genre: {
+      type: [String],
+      required: true,
+      index: true,
+    },
+    country: {
+      type: [String],
+      required: true,
+    },
+    colour: {
+      type: [String],
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    isFavourite: { type: Boolean, default: false },
+    additionalDetails: {
+      overview: {
+        type: String,
+      },
+      popularity: {
+        type: String,
+      },
+      adult: {
+        type: Boolean,
+      },
+    },
+  },
+  { timestamps: true, _id: false }
+);
+
+const connection = getMoviesDb();
+
+export default connection.model("Movie", movieSchema);
---
+++ src/seeder/moviesSeeder.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import fs from "fs";
+import csv from "csv-parser";
+import MovieModel from "../models/movies";
+
+const importCSV = async () => {
+  const filePath = path.join(__dirname, "./1000GreatestFilms.csv");
+
+  const movies:any[] = [];
+
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (row) => {
+      console.log({ row });
+      const movie = {
+        title: row?.["Title"].split(",").length > 2 ?row?.["Title"]:row?.["Title"]?.split(",").reverse().join(" ").trim(),
+        genre: row?.["Genre"].split("-"),
+        year: parseInt(row?.["Year"], 10),
+        colour: row?.["Colour"].split("-"),
+        country: row?.["Country"]?.split("-"),
+        director: row?.["Director"]?.split(",").reverse().join(" ").trim(),
+      };
+      movies.push({
+        updateOne: {
+          filter: { title: movie.title ,year:movie.year},
+          update: { $setOnInsert: movie },
+          upsert: true,
+        },
+      });
+    })
+    .on('end', async () => {
+
+        await MovieModel.bulkWrite(movies);
+
+        console.log('CSV file successfully processed and data imported.');
+
+      });;
+};
+export default importCSV;
